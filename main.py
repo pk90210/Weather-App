0561ab5d915652637c54210eb59b785218ca886e
pk90210 <pkahara908@gmail.com>
2026-04-23 10:23:48 +0300

    Modified main.py

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ import os
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
-city = 'Nairobi'
+city = input("Enter City: ")
 url =  f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}'
 
 response = requests.get(url)
 data = response.json()
 
-print(data)
+if "error" in data:
+    print(f"Error: {data['error']['message']}")
+else:
+	print(f"City: {data['location']['name']}")
+	print(f"Temperature: {data['current']['temp_c']}°C")
+	print(f"Condition: {data['current']['condition']['text']}")
+	print(f"Humidity: {data['current']['humidity']}%")
+	print(f"Wind Speed: {data['current']['wind_kph']} kph")
