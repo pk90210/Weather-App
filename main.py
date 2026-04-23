9c29f46866961832f84f7af4e8b00b1eec9979e6
pk90210 <pkahara908@gmail.com>
2026-04-23 09:59:47 +0300

    Added an API call

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import requests
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+city = 'Nairobi'
+url =  f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}'
+
+response = requests.get(url)
+data = response.json()
+
+print(data)
